9e1f5e4bd28974c8a277a88abc7bdb3965a1286f
xiaojin <33309882+xiaojin525@users.noreply.github.com>
2019-10-16 15:00:45 +0800

    Update delNs.sh

--- delNamespace/delNs.sh
+++ delNamespace/delNs.sh
@@ -6,6 +6,7 @@ do
 	echo Delete Terminating namesapce $i ;
 	kubectl get namespace $i -o json > tmp.json
 	cat tmp.json |jq '.spec.finalizers=[]' |jq '.metadata.finalizers=[]' >tmp2.json
+	#cat tmp.json |jq '.spec.finalizers=[]' >tmp2.json
 	#cat tmp.json |jq '.metadata.finalizers=[]' >tmp2.json
 	curl -k -H "Content-Type: application/json" -X PUT --data-binary @tmp2.json http://127.0.0.1:8001/api/v1/namespaces/$i/finalize
 	echo -e "\n"
