4a416c642a0c6c7942e1c959542452eb9a349479
xiaojin <33309882+xiaojin525@users.noreply.github.com>
2019-12-19 15:00:52 +0800

    Create config.sh

---
+++ kubeConfig/config.sh
@@ -0,0 +1,11 @@
+#cd $HOME/.kube/config
+echo "=== start ==="
+kc=""
+for config in $(ls configfile)
+do
+    echo add ./configfile/$config to config
+    kc=$kc:$config
+done
+#echo ${kc#*:}
+KUBECONFIG=${kc#*:} kubectl config view --flatten > config
+echo "=== done ==="
